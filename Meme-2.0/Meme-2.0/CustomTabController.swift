a2aeb40db69c27bfbd2f847a1b83819fceacb1fd
surnan <forsuresh@yandex.com>
2018-12-09 17:46:56 -0500

    Properly pre-loading MemeArray that was declared in AppDelegate

--- Meme-2.0/Meme-2.0/CustomTabController.swift
+++ Meme-2.0/Meme-2.0/CustomTabController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class CustomTabController: UITabBarController {
 
+    
+    var dummyTextField: UITextField = {
+        var textField = UITextField()
+        textField.text = "!!!WELCOME TO THE JUNGLE!!!"
+        return textField
+    }()
+    
+    var dummyImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.backgroundColor = UIColor.yellow
+        return imageView
+    }()
+    
+    var dummyImage = UIImage(imageLiteralResourceName: "cry")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,9 +35,16 @@ class CustomTabController: UITabBarController {
         let secondController = ContainerController()
         secondController.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 1)
         
-        
-        
         let tabBarList = [firstController, secondController]
         viewControllers = tabBarList
+        
+        
+        let myAppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let currentMeme = Meme(topTxtField: dummyTextField, bottomTxtField: dummyTextField, originalImageView: dummyImageView, memeImage: dummyImage)
+        myAppDelegate.memes.append(currentMeme)
+        myAppDelegate.memes.append(currentMeme)
+        myAppDelegate.memes.append(currentMeme)
+        myAppDelegate.memes.append(currentMeme)
+        myAppDelegate.memes.append(currentMeme)
     }
 }
--- Meme-2.0/Meme-2.0/TableController.swift
+++ Meme-2.0/Meme-2.0/TableController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class TableController: UITableViewController {
     
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        let myAppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let currentMemeArray = myAppDelegate.memes
+        print("Number of Elements in myAppDelegate.memes = \(currentMemeArray.count) \n\n")
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.green
         tabBarController?.title = "Sent Memes"
         tabBarController?.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleThis))
+        
+       
     }
     
     
     @objc func handleThis(){
         print("Hello")
-        
         present(MemeController(), animated: true)
-        
     }
-    
 }
